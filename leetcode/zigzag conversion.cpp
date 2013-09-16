09d3afbbef6f6fdf38d67ac8e314d44ba15e69f2
Andy <worton.wu@gmail.com>
2013-09-16 23:53:08 +0800

    zigzag conversion

--- leetcode/zigzag conversion.cpp
+++ leetcode/zigzag conversion.cpp
@@ -7,31 +7,19 @@ class Solution {
 public:
 	string convert(string s, int nRows) {
 		if (nRows == 1) return s;
-		int len = s.length();
-		int width = (len - nRows) / (nRows - 1);
-		
-		char** c = new char*[nRows];
-		for (int i = 0; i < nRows; ++i) {
-			c[i] = new char[width];
-			memset(c[i], 0, width*sizeof(char));
-		}
-		int dx[] = {1, -1};
-		int dy[] = {0,  1};
-		int count = 0, i = 0, j = 0, id = 0;
-		while (count <= len) {
-			c[i][j] = s[count++];
-			if (id == 1 && i == 0) id = 1 - id;
-			if (id == 0 && i == nRows-1) id = 1 - id;
-			i += dx[id];
-			j += dy[id];
-		}
-		string ans;
-		for (int i = 0; i < nRows; ++i){
-			for (int j = 0; j < width; ++j){
-				if (c[i][j]) ans += c[i][j];
+		string ret;
+		int n = s.length();
+		int largeGap = (nRows << 1) - 2;
+		for (int r = 0; r < nRows; ++r) {
+			int i = r;
+			int gap = largeGap - 2*i;
+			while (i < n){
+				ret += s[i];
+				i += (gap == 0 ? largeGap : gap);
+				gap = largeGap - gap;
 			}
 		}
-		return ans;
+		return ret;
 	}
 };
 
